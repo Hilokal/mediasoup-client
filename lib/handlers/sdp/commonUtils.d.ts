b604baa934fe4db03049673eb77020c2defb1cda
Jonathan Baudanza <jon@jonb.org>
2024-11-18 11:54:14 +0900

    gitpkg

---
+++ lib/handlers/sdp/commonUtils.d.ts
@@ -0,0 +1,44 @@
+import { DtlsParameters } from '../../Transport';
+import { RtpCapabilities, RtpHeaderExtensionParameters, RtpParameters, RtpHeaderExtensionUri } from '../../RtpParameters';
+/**
+ * This function must be called with an SDP with 1 m=audio and 1 m=video
+ * sections.
+ */
+export declare function extractRtpCapabilities({ sdpObject, }: {
+    sdpObject: any;
+}): RtpCapabilities;
+export declare function extractDtlsParameters({ sdpObject, }: {
+    sdpObject: any;
+}): DtlsParameters;
+export declare function getCname({ offerMediaObject, }: {
+    offerMediaObject: any;
+}): string;
+/**
+ * Apply codec parameters in the given SDP m= section answer based on the
+ * given RTP parameters of an offer.
+ */
+export declare function applyCodecParameters({ offerRtpParameters, answerMediaObject, }: {
+    offerRtpParameters: RtpParameters;
+    answerMediaObject: any;
+}): void;
+/**
+ * Adds the given RTP extension to the given SDP media object and returns a
+ * RtpHeaderExtensionParameters object.
+ * If the extension is already present, this function doesn't add anything and
+ * doesn't return anything.
+ */
+export declare function addRtpExtensionToMediaObject({ mediaObject, uri }: {
+    mediaObject: any;
+    uri: RtpHeaderExtensionUri;
+}): RtpHeaderExtensionParameters | undefined;
+/**
+ * Adds the given RTP extension to the given RTP parameters.
+ * If the extension is already present (with same id), this function doesn't
+ * add anything. If the extension is present with a different id, then existing
+ * one is removed and the new one is added.
+ */
+export declare function addRtpExtensionToRtpParameters({ rtpParameters, extension }: {
+    rtpParameters: RtpParameters;
+    extension: RtpHeaderExtensionParameters;
+}): void;
+//# sourceMappingURL=commonUtils.d.ts.map
